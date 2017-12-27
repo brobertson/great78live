b2cb53e3a4f78b9b9ce228bf9139c34bbc8465f3
Bruce Robertson <brobertson@mta.ca>
2017-12-27 13:06:44 -0400

    provide info on how to get keys

--- great78player_config_example.py
+++ great78player_config_example.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#go to the twitter site here, https://apps.twitter.com/app/new
+#and sign up yourself for an app API.
 #provide the actual values below, and rename this file to 'great78player_config.py'
 consumer_key = 'CONSUMER_KEY'
 consumer_secret = 'CONSUMER_SECRET'
