e84d37d0ac7663a860e35192213093ac50f10055
Bruce Robertson <brobertson@mta.ca>
2017-12-27 12:11:05 -0400

    Update great78player_config_example.py

--- great78player_config_example.py
+++ great78player_config_example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#provide the actual values below, and rename this file to 'great78player_config.py'
 consumer_key = 'CONSUMER_KEY'
 consumer_secret = 'CONSUMER_SECRET'
 access_token = 'ACCESS_TOKEN'
